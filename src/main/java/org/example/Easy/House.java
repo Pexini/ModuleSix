c47282224a695ce55495c643ceb283cb40ec2833
Pavel Davydov <Davydov.works@gmail.com>
2024-02-21 03:53:16 -0500

    Add README

---
+++ src/main/java/org/example/Easy/House.java
@@ -0,0 +1,11 @@
+package org.example.Easy;
+
+public class House {
+    String  address;
+    int constructionYear;
+
+    public House(String address, int constructionYear){
+        this.address = address;
+        this.constructionYear = constructionYear;
+    }
+}
