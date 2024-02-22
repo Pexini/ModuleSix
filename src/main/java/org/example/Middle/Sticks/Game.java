8a297862bdef20d8bd2cd66133c17165660b2853
Pavel Davydov <Davydov.works@gmail.com>
2024-02-22 03:51:50 -0500

    Middle task3

--- src/main/java/org/example/Middle/Sticks/Game.java
+++ src/main/java/org/example/Middle/Sticks/Game.java
@@ -5,16 +5,19 @@ import java.util.Scanner;
 
 public class Game {
     private static Random random = new Random();
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        StickPrinter stickPrinter = new StickPrinter();
         Scanner scanner = new Scanner(System.in);
 
         int sticks = 20;
 
         System.out.println("Добро пожаловать в игру \"Палочки\"!");
-        System.out.println( "У нас 20 палочек, сколько вы хотите убрать?");
-        while (sticks > 0) {
-            int playerMove = getPlayerMove();
+        stickPrinter.printSticks(sticks);
+        System.out.println("У нас " + sticks + " сколько вы хотите убрать?");
+
+        while (sticks > 1) {
+            int playerMove = getPlayerMove(sticks);
             sticks -= playerMove;
             if (sticks <= 1) {
                 System.out.println("Поздравляю, вы победили! Осталась последняя палочка.");
@@ -22,20 +25,26 @@ public class Game {
             }
             int computerMove = getComputerMove(sticks);
             sticks -= computerMove;
+
             System.out.println("Компьютер делает ход и убирает " + computerMove + " палочек");
+            stickPrinter.printSticks(sticks);
+            if (sticks<=1){
+                System.out.println("Увы, вы проиграли.");
+            }
             System.out.println("Осталось палочек: " + sticks);
 
         }
 
     }
 
-    private static int getPlayerMove() {
+
+    private static int getPlayerMove(int sticks) {
         Scanner scanner = new Scanner(System.in);
         int playMove;
         do {
             System.out.println("Ваш ход ( выберите сколько палочек хотите убрать от 1 до 3)? ");
             playMove = scanner.nextInt();
-        } while (playMove < 1 || playMove > 3);
+        } while (playMove < 1 || playMove > 3 || playMove >= sticks);
         return playMove;
     }
 
---
+++ src/main/java/org/example/Middle/Sticks/StickPrinter.java
@@ -0,0 +1,11 @@
+package org.example.Middle.Sticks;
+
+public class StickPrinter {
+    public static void printSticks(int count){
+        for (int i = 0; i < count; i++) {
+            System.out.print(" | ");
+        }
+        System.out.println();
+    }
+
+}
