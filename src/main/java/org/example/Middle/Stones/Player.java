dccb6e18c8e21431a9e4b520273b9660f9af4e32
Pavel Davydov <Davydov.works@gmail.com>
2024-02-22 04:50:05 -0500

    Middle task4

--- src/main/java/org/example/Middle/Stones/Player.java
+++ src/main/java/org/example/Middle/Stones/Player.java
@@ -1,15 +1,19 @@
 package org.example.Middle.Stones;
 
 public class Player {
-
-
     private String name;
+    private int counter;
 
-    public Player(String name) {
+    public Player(String name, int counter) {
         this.name = name;
+        this.counter = counter;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getCounter() {
+        return counter;
     }
 }
